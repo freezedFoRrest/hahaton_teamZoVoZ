8f304a338e9e697affa367d7d993a4d068a9a3b0
Egor Tarantsov <benzogangegorka@gmail.com>
2026-05-29 20:53:12 +0300

    Добавили парсинг файлов с форматом json

--- src/parser.py
+++ src/parser.py
@@ -1,28 +1,34 @@
 class Letter:
-    def __init__(self, receiver, sender, date, subject, data, file_name):
+    def __init__(self, receiver, sender, date, subject, main_body, file_name):
         self.receiver = receiver
         self.sender = sender
         self.date = date
         self.subject = subject
-        self.data = data
+        self.main_body = main_body
         self.name = file_name
 
 class Parser:
-    def __init__(self, file_path, extension):
+    def __init__(self, file_path):
         self.receiver = None
         self.sender = None
         self.date = None
+        self.file_path = file_path
         self.subject = None
         self.main_body = []
         self.file_name = file_path.name
         self.extension = extension
         self.letter = None
-        if self.extension == ".txt":
+
+
+    def parse(self, extension):
+        if extension == ".txt":
             self.parse_txt()
-            
+        elif extension == ".json":
+            self.parse_json()
+
     def parse_txt(self):
         try:
-            with (open(self.name, 'r', encoding='utf-8') as file):
+            with (open(self.file_path, 'r', encoding='utf-8') as file):
                 for line in file:
                     line = line.strip()
                     if line.startswith("From:") or line.startswith("От кого:"):
@@ -45,6 +51,26 @@ class Parser:
 
         except Exception as e:
             raise RuntimeError(f"Ошибка при разборе {self.file_name}: {e}")
+    
+    def parse_json(self):
+        try:
+            with open (self.file_path, 'r', encoding='utf-8') as file:
+                data = json.load(file)
+            self.sender = data.get("from") or data.get("From") or data.get("от кого") or data.get("От кого")
+            self.receiver = data.get("To") or data.get("to") or data.get("Кому") or data.get("кому")
+            self.date = data.get("Date") or data.get("date") or data.get("Дата") or data.get("дата")
+            self.subject = data.get("Subject") or data.get("subject") or data.get("тема") or data.get("Тема")
+            self.main_body = data.get("body") or data.get("Body") or data.get("текст")
+            self.makeLetter()
+        except FileNotFoundError:
+            raise ValueError(f"Файл {self.file_name} не найден")
+
+        except json.JSONDecodeError as e:
+            raise ValueError(f"Не удалось разобрать JSON в файле {self.file_name}: {e}")
+
+        except Exception as e:
+            raise RuntimeError(f"Ошибка при разборе {self.file_name}: {e}")
+
     
     def makeLetter(self):
         letter = Letter(self.receiver, self.sender, self.date, self.subject, self.main_body, self.file_name)
