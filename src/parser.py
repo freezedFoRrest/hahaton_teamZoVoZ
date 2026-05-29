22ada2fcf139e3b8b352c142c176453f1331cc73
Egor Tarantsov <benzogangegorka@gmail.com>
2026-05-29 19:34:33 +0300

    Запарсил из файла данные и проклассифицировал их в отдельные переменные

--- src/parser.py
+++ src/parser.py
@@ -19,3 +19,29 @@ class Parser:
         self.letter = None
         if self.extension == ".txt":
             self.parse_txt()
+            
+    def parse_txt(self):
+        try:
+            with (open(self.name, 'r', encoding='utf-8') as file):
+                for line in file:
+                    line = line.strip()
+                    if line.startswith("From:") or line.startswith("От кого:"):
+                        self.sender = line[line.index(":")+1:].strip()
+                    elif line.startswith("To:") or line.startswith("Кому:"):
+                        self.receiver = line[line.index(":") + 1:].strip()
+                    elif line.startswith("Date:") or line.startswith("Дата:"):
+                        self.date = line[line.index(":") + 1:].strip()
+                    elif line.startswith("Subject:") or line.startswith("Тема:"):
+                        self.subject = line[line.index(":") + 1:].strip()
+                    else:
+                        self.main_body.append(line)
+                self.makeLetter()
+
+        except FileNotFoundError:
+            raise ValueError(f"Файл {self.file_name} не найден")
+
+        except UnicodeDecodeError:
+            raise ValueError(f"Не удалось декодировать {self.file_name} - проверьте кодировку")
+
+        except Exception as e:
+            raise RuntimeError(f"Ошибка при разборе {self.file_name}: {e}")
